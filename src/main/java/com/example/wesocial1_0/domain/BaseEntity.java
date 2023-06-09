08ff859756cf1f56a0e677f917620fcd593297c3
Pakshaev <pakshaev2003@gmail.com>
2023-06-10 02:23:05 +0400

    created date field changed

--- src/main/java/com/example/wesocial1_0/domain/BaseEntity.java
+++ src/main/java/com/example/wesocial1_0/domain/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.example.wesocial1_0.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -15,15 +17,12 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @CreatedDate
-    @Column(name = "created")
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreationTimestamp
     private Date created;
 
     @LastModifiedDate
     @Column(name = "updated")
     private Date updated;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status")
-    private Status status;
 }
